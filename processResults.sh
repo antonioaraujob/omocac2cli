8127b3d5d7781704edb3ec62c406d8774eb14d07
antonioaraujob <antonioaraujob@gmail.com>
2015-03-17 17:39:44 -0430

    Cambios en el script processResults.sh para generar el archivo LaTeX con dos tablas: la de individuos finales y la de individuos ordenados por genes de acuerdo a la latencia de mayor a menor.

--- processResults.sh
+++ processResults.sh
@@ -15,12 +15,13 @@
 # iniciar el archivo latex
 echo "\documentclass{article}
 \usepackage{fullpage}
-\usepackage[a4paper,margin=1in,landscape]{geometry}
+\usepackage[a4paper,margin=10mm,landscape]{geometry}
 
 \usepackage{tabularx}
+\usepackage{tablefootnote}
 
 \title{Resultados de ejecucion}
-\author{PGCOMP}
+%\author{PGCOMP}
 \date{}
 
 \begin{document}
@@ -29,7 +30,7 @@ echo "\documentclass{article}
 \begin{table}[h]
 \centering
 \caption{Channel sequences}
-\label{tab:summary}
+\label{tab:sequences}
 
 \begin{tabularx}{\textwidth}{XXXXXXXXXXXrr}
 
@@ -40,7 +41,7 @@ echo "\documentclass{article}
 " > /tmp/salida.tex
 
 
-# procesar cada linea de secuencia de canales
+# procesar cada linea de secuencia de canales del archivo individuosFrenteParetoOriginal.txt
 awk ' 
 
 BEGIN { 
@@ -61,6 +62,63 @@ echo "\hline
 \end{tabularx}
 
 \end{table}
+
+" >> /tmp/salida.tex
+
+# agregar la tabla correspondiente a los individuos con canales ordenados por mayor latencia
+
+echo "
+\begin{table}[h]
+\centering
+\caption{Channel sequences ordered by channels latency}
+\label{tab:sequencesorderbylatency}
+
+\begin{tabularx}{\textwidth}{XXXXXXXXXXXrr}
+
+%\begin{tabular}{lrrrrrrrrrrrr}
+\hline
+\multicolumn{11}{l}{Ch$<\$min,max\$>\rightarrow\$AP} & \$FO_1\$ & \$FO_2\$ \\""\\
+\hline
+" >> /tmp/salida.tex
+
+# procesar cada linea de secuencia de canales del archivo individuosFrenteParetoOriginalPorLatencia.txt
+awk ' 
+
+BEGIN { 
+	FS=",";  
+}  
+
+{ 
+	print $1"$<$"$2","$3"$>\\rightarrow$"$4" & " $5"$<$"$6","$7"$>\\rightarrow$"$8" & " $9"$<$"$10","$11"$>\\rightarrow$"$12" & " \
+	$13"$<$"$14","$15"$>\\rightarrow$"$16" & " $17"$<$"$18","$19"$>\\rightarrow$"$20" & " $21"$<$"$22","$23"$>\\rightarrow$"$24" & " \
+	$25"$<$"$26","$27"$>\\rightarrow$"$28" & " $29"$<$"$30","$31"$>\\rightarrow$"$32" & " $33"$<$"$34","$35"$>\\rightarrow$"$36" & " \
+	$37"$<$"$38","$39"$>\\rightarrow$"$40" & " $41"$<$"$42","$43"$>\\rightarrow$"$44" & " $45" & "$46 "\134\134" ;
+}' /tmp/individuosFrenteParetoOriginalPorLatencia.txt >> /tmp/salida.tex
+
+# finalizar la tabla de latex
+echo "
+\hline
+%\end{tabular}
+
+\end{tabularx}
+
+\end{table}
+
+
+" >> /tmp/salida.tex
+
+
+# procesar cada linea de secuencia de canales del archivo individuosFrenteParetoOriginalPorLatencia.txt
+awk ' 
+
+BEGIN {}  
+
+{ 
+	print $0 "\n";
+}' /tmp/settings.ini >> /tmp/salida.tex
+
+
+echo "
 
 \end{document}" >> /tmp/salida.tex
 
