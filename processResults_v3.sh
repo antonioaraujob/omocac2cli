ea28eecf7f302dde81cc52437217f1d95a3ae083
antonioaraujob <antonioaraujob@gmail.com>
2015-03-18 17:23:03 -0430

    Agregada modificación en el script processResults_v3.sh para generar la tabla de individuos ordenada por latencia de mayor a menor en formato LaTeX.

--- processResults_v3.sh
+++ processResults_v3.sh
@@ -238,19 +238,151 @@ echo "
 awk ' 
 
 BEGIN { 
-	FS=",";  
+	FS=","; gen1="";gen2="";gen3="";gen4="";gen5="";gen6="";gen7="";gen8="";gen9="";gen10="";gen11="";
 }  
 
 { 
-#	print $1"$<$"$2","$3"$>\\rightarrow$"$4" & " $5"$<$"$6","$7"$>\\rightarrow$"$8" & " $9"$<$"$10","$11"$>\\rightarrow$"$12" & " \
-#	$13"$<$"$14","$15"$>\\rightarrow$"$16" & " $17"$<$"$18","$19"$>\\rightarrow$"$20" & " $21"$<$"$22","$23"$>\\rightarrow$"$24" & " \
-#	$25"$<$"$26","$27"$>\\rightarrow$"$28" & " $29"$<$"$30","$31"$>\\rightarrow$"$32" & " $33"$<$"$34","$35"$>\\rightarrow$"$36" & " \
-#	$37"$<$"$38","$39"$>\\rightarrow$"$40" & " $41"$<$"$42","$43"$>\\rightarrow$"$44" & " $45" & "$46 "\134\134" ;
 
-	print "$\\textbf{"$1"}^{"$2+$3"}$\\newline$<$"$2","$3"$>\\rightarrow$"$4" & " "$\\textbf{"$5"}^{"$2+$3+$6+$7"}$\\newline$<$"$6","$7"$>\\rightarrow$"$8" & " "$\\textbf{"$9"}^{"$2+$3+$6+$7+$10+$11"}$\\newline$<$"$10","$11"$>\\rightarrow$"$12" & " \
-	"$\\textbf{"$13"}^{"$2+$3+$6+$7+$10+$11+$14+$15"}$\\newline$<$"$14","$15"$>\\rightarrow$"$16" & " "$\\textbf{"$17"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19"}$\\newline$<$"$18","$19"$>\\rightarrow$"$20" & " "$\\textbf{"$21"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23"}$\\newline$<$"$22","$23"$>\\rightarrow$"$24" & " \
-	"$\\textbf{"$25"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27"}$\\newline$<$"$26","$27"$>\\rightarrow$"$28" & " "$\\textbf{"$29"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31"}$\\newline$<$"$30","$31"$>\\rightarrow$"$32" & " "$\\textbf{"$33"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35"}$\\newline$<$"$34","$35"$>\\rightarrow$"$36" & " \
-	"$\\textbf{"$37"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35+$38+$39"}$\\newline$<$"$38","$39"$>\\rightarrow$"$40" & " "$\\textbf{"$41"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35+$38+$39+$42+$43"}$\\newline$<$"$42","$43"$>\\rightarrow$"$44" & " $45" & "$46 "\134\134" ;
+	if ($4=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen1="\\textit{"$1"}";
+		#gen1="\\textit{\\textbf{"$1"}\\newline$<$"$2","$3"$>\\rightarrow$"$4"}";
+		gen1="\\textit{$\\textbf{"$1"}^{"$2+$3"}$\\newline$<$"$2","$3"$>\\rightarrow$"$4"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen1="\\textbf{"$1"}";
+		#gen1="\\textbf{"$1"}\\newline$<$"$2","$3"$>\\rightarrow$"$4;
+		gen1="$\\textbf{"$1"}^{"$2+$3"}$\\newline$<$"$2","$3"$>\\rightarrow$"$4;
+	}
+	if ($8=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen2="\\textit{"$5"}";
+		gen2="\\textit{$\\textbf{"$5"}^{"$2+$3+$6+$7"}$\\newline$<$"$6","$7"$>\\rightarrow$"$8"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen2="\\textbf{"$5"}";
+		gen2="$\\textbf{"$5"}^{"$2+$3+$6+$7"}$\\newline$<$"$6","$7"$>\\rightarrow$"$8;
+	}
+
+	if ($12=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen3="\\textit{"$9"}";
+		#$\\textbf{"$9"}^{"$2+$3+$6+$7+$10+$11"}$\\newline$<$"$10","$11"$>\\rightarrow$"$12" & " \
+		gen3="\\textit{$\\textbf{"$9"}^{"$2+$3+$6+$7+$10+$11"}$\\newline$<$"$10","$11"$>\\rightarrow$"$12"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen3="\\textbf{""$9}";
+		gen3="$\\textbf{"$9"}^{"$2+$3+$6+$7+$10+$11"}$\\newline$<$"$10","$11"$>\\rightarrow$"$12;
+	}
+	if ($16=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen4="\\textit{"$13"}";
+		gen4="\\textit{$\\textbf{"$13"}^{"$2+$3+$6+$7+$10+$11+$14+$15"}$\\newline$<$"$14","$15"$>\\rightarrow$"$16"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen4="\\textbf{"$13"}";
+		gen4="$\\textbf{"$13"}^{"$2+$3+$6+$7+$10+$11+$14+$15"}$\\newline$<$"$14","$15"$>\\rightarrow$"$16;
+	}
+	if ($20=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen5="\\textit{"$17"}";
+		gen5="\\textit{$\\textbf{"$17"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19"}$\\newline$<$"$18","$18"$>\\rightarrow$"$20"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen5="\\textbf{"$17"}";
+		gen5="$\\textbf{"$17"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19"}$\\newline$<$"$18","$19"$>\\rightarrow$"$20;
+	}
+	if ($24=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen6="\\textit{"$21"}";
+		gen6="\\textit{$\\textbf{"$21"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23"}$\\newline$<$"$22","$23"$>\\rightarrow$"$24"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen6="\\textbf{"21"}";
+		gen6="$\\textbf{"$21"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23"}$\\newline$<$"$22","$23"$>\\rightarrow$"$24;
+	}
+	if ($28=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen7="\\textit{"$25"}";
+		gen7="\\textit{$\\textbf{"$25"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27"}$\\newline$<$"$26","$27"$>\\rightarrow$"$28"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen7="\\textbf{"$25"}";
+		gen7="$\\textbf{"$25"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27"}$\\newline$<$"$26","$27"$>\\rightarrow$"$28;
+	}
+	if ($32=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen8="\\textit{"$29"}";
+		gen8="\\textit{$\\textbf{"$29"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31"}$\\newline$<$"$30","$31"$>\\rightarrow$"$32"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen8="\\textbf{"$29"}";
+		gen8="$\\textbf{"$29"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31"}$\\newline$<$"$30","$31"$>\\rightarrow$"$32;
+	}
+	if ($36=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen9="\\textit{"$33"}";
+		gen9="\\textit{$\\textbf{"$33"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35"}$\\newline$<$"$34","$35"$>\\rightarrow$"$36"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen9="\\textbf{"$33"}";
+		gen9="$\\textbf{"$33"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35"}$\\newline$<$"$34","$35"$>\\rightarrow$"$36;
+	}
+	if ($40=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen10="\\textit{"$37"}";
+		gen10="\\textit{$\\textbf{"$37"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35+$38+$39"}$\\newline$<$"$38","$39"$>\\rightarrow$"$40"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen10="\\textbf{"$37"}";
+		gen10="$\\textbf{"$37"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35+$38+$39"}$\\newline$<$"$38","$39"$>\\rightarrow$"$40;
+	}
+	if ($44=="0")
+	{
+		#gen1="$\textit{$\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5}";
+		#gen11="\\textit{"$41"}";
+		gen11="\\textit{$\\textbf{"$41"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35+$38+$39+$42+$43"}$\\newline$<$"$42","$43"$>\\rightarrow$"$44"}";
+	}
+	else
+	{
+		#gen1="$\\textbf{10}^{70}$\newline$<$7,63$>\rightarrow$5";
+		#gen11="\\textbf{"$41"}";
+		gen11="$\\textbf{"$41"}^{"$2+$3+$6+$7+$10+$11+$14+$15+$18+$19+$22+$23+$26+$27+$30+$31+$34+$35+$38+$39+$42+$43"}$\\newline$<$"$42","$43"$>\\rightarrow$"$44;
+	}
+	
+		
+	print gen1" & "gen2" & "gen3" & "gen4" & "gen5" & "gen6" & "gen7" & "gen8" & "gen9" & "gen10" & "gen11 " & " $45" & "$46 "\134\134" ;
+
 }' /tmp/individuosFrenteParetoOriginalPorLatencia.txt >> /tmp/salida.tex
 
 # finalizar la tabla de latex
